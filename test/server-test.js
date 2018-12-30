3993b4fd6548da51df08e328b3bbf06feda4014c
Angelo Martin <angeloqmartin@gmail.com>
2018-12-30 15:40:27 -0800

    added test for POST request

--- test/server-test.js
+++ test/server-test.js
@@ -27,6 +27,30 @@ describe("Shopping List", function() {
             expect(res.body).to.be.a("array");
             expect(res).to.be.json;
             expect(res.body.length).to.be.at.least(1);
+            const expectedKeys = ["id", "title", "content", "author"];
+            res.body.forEach(function(item) {
+              expect(item).to.be.a("object");
+              expect(item).to.include.keys(expectedKeys);
+            });
         });
     });  
+
+    // test strategy:
+    //  1. make a POST request with data for a new item
+    //  2. inspect response object and prove it has right
+    //  status code and that the returned object has an `id`
+
+    it("should add an blog post POST", function() {
+        const newBlogPost = {title: "test for POST", content: "test passed", author: "root"}
+        return chai
+        .request(app)
+        .post("/blog-post")
+        .send(newBlogPost).then(function(res) {
+            expect(res).to.have.status(201);
+            expect(res).to.be.json;
+            expect(res.body).to.be.a("object");
+            expect(res.body).to.include.keys("title", "content", "author");
+            expect(res.body.id).not.to.equal(null);
+        });
+    });
   });
