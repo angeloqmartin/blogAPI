747ff35e1f2f79ec8810e2efa2a9b6b51ab437ed
Angelo Martin <angeloqmartin@gmail.com>
2018-12-30 15:13:31 -0800

    created test for GET request to /blog-post

--- test/server-test.js
+++ test/server-test.js
@@ -14,39 +14,19 @@ describe("Shopping List", function() {
     });
     
     // test strategy:
-    //   1. make request to `BlogPosts`
+    //   1. make request to `/blog-post`
     //   2. inspect response object and prove has right code and have
     //   right keys in response object.
 
     it("should list blog post on GET", function() {
       return chai
         .request(app)
-        .get("/BlogPosts")
+        .get("/blog-post")
         .then(function(res) {
-            expect(res.body).to.be.a("object");
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.a("array");
+            expect(res).to.be.json;
+            expect(res.body.length).to.be.at.least(1);
         });
-    });
-
-    // test strategy:
-    //  1. make POST request with data for a new item
-    //  2. inspect response object and prove it has right
-    //  2a. status code and that the returned object has an `id`
-
-    it("should add an item on git ", function() {
-        const newItem = { name: "coffee", ingredients: ["water", "beans"] };
-        return chai
-        .request(app)
-        .post("/recipes")
-        .send(newItem)
-        .then(function(res) {
-            expect(res).to.have.status(201);
-            // expect(res).to.be.json;
-            // expect(res).to.be.a("object");
-            // expect(res.body.id).to.not.equal(null);
-            // expect(res.body).to.include.keys('id', 'name', 'ingredients');
-            // expect(res.body.name).to.be.equal(newItem.name);
-            // expect(res.body.ingredients).to.be.a('array');
-            // expect(res.body.ingredients).to.include.members(newItem.ingredients);
-        }); 
-    });    
+    });  
   });
