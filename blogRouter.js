1975e58f37d28ee71261f745e9472cfd36de7989
Angelo Martin <angeloqmartin@gmail.com>
2018-12-29 20:54:25 -0800

    installed npm chai-http

--- blogRouter.js
+++ blogRouter.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 const bodyParser = require('body-parser')
-
 const jsonParser = bodyParser.json();
-
-
 const {BlogPosts} = require('./models');
 
 BlogPosts.create(
---
+++ test/server-test.js
@@ -0,0 +1,37 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const { app, runServer, closeServer } = require("../server");
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe("Shopping List", function() {
+    before(function() {
+      return runServer();
+    });
+    after(function() {
+      return closeServer();
+    });
+    
+    // test strategy:
+    //   1. make request to `BlogPosts`
+    //   2. inspect response object and prove has right code and have
+    //   right keys in response object.
+
+    it("should list blog post on GET", function() {
+      return chai
+        .request(app)
+        .get("/BlogPosts")
+        .then(function(res) {
+          expect(res).to.have.status(200);
+          expect(res).to.be.json;
+          expect(res.body).to.be.a("array");
+          expect(res.body.length).to.be.at.least(1);
+          const expectedKeys = ["title", "content", "author"];
+          res.body.forEach(function(item) {
+            expect(item).to.be.a("object");
+            expect(item).to.include.keys(expectedKeys);
+          });
+        });
+    });
+  });
