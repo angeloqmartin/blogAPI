a6449432e042ed1b9ed137ae4db8bdf9eaeecbd2
Angelo Martin <angeloqmartin@gmail.com>
2019-01-07 08:36:15 -0800

    added Mongoose/ endpoints to app

--- models.js
+++ models.js
@@ -1,28 +1,10 @@
-// app requirements => 
-
-// > Use npm init to initialize your project
-// > Install uuid (Node package) run npm install uuid within 
-// > App should support the four CRUD operations for a blog posts resource
-    // GET and POST requests should go to /blog-posts.
-    // DELETE and PUT requests should go to /blog-posts/:id.
-// > Use Express router and modularize routes to /blog-posts
-// > Add a couple of blog posts on server load
-
-// Note => 
-
-// to create a new blog post, supply a title, content, an author name
-// and (optionally) a publication date
-
 const uuid = require('uuid');
 
-// this module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
-
-// don't worry to much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
+// connent to Mongo
+const {DATABASE_URL} = require('./config');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(DATABASE_URL);
 
 function StorageException(message) {
   this.message = message;
@@ -41,7 +23,7 @@ const BlogPosts = {
     this.posts.push(post);
     return post;
   },
-  
+
   get: function(id = null) {
     // if id passed in, retrieve single post,
     // otherwise send all posts.
@@ -53,24 +35,25 @@ const BlogPosts = {
     return this.posts.sort(function(a, b) {
       return b.publishDate - a.publishDate;
     });
-  },
-
+ }, 
+  
   delete: function(id) {
-    const postIndex = this.posts.findIndex(post => post.id === id);
+    const postIndex = this.posts.findIndex(
+      post => post.id === id);
     if (postIndex > -1) {
       this.posts.splice(postIndex, 1);
     }
   },
-
   update: function(updatedPost) {
     const { id } = updatedPost;
-    const postIndex = this.posts.findIndex(post => post.id === updatedPost.id);
+    const postIndex = this.posts.findIndex(
+      post => post.id === updatedPost.id);
     if (postIndex === -1) {
       throw StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`
-      );
+        `Can't update item \`${id}\` because doesn't exist.`)
     }
-    this.posts[postIndex] = Object.assign(this.posts[postIndex], updatedPost);
+    this.posts[postIndex] = Object.assign(
+      this.posts[postIndex], updatedPost);
     return this.posts[postIndex];
   }
 };
@@ -79,6 +62,23 @@ function createBlogPostsModel() {
   const storage = Object.create(BlogPosts);
   storage.posts = [];
   return storage;
+};
+
+BlogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+BlogPostSchema.methods.apiRepr = function() {
+  return { 
+    id: this._id,
+    author: this.authorName,
+    content: this.content,
+    title: this.title,
+    published: this.published
+  };
 }
 
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
 module.exports = { BlogPosts: createBlogPostsModel() };
+module.exports = {BlogPost};
--- server.js
+++ server.js
@@ -1,46 +1,129 @@
+const bodyParser = require('body-parser');
 const express = require("express");
+const mongoose = require('mongoose');
 const morgan = require("morgan");
-
 const blogRouter = require("./blogRouter");
+const {DATABASE_URL, PORT} = require('./config');
+const {BlogPost} = require('./models');
 const app = express();
 
-app.use(express.json());
-
 app.use(morgan("common"));
-
+app.use(bodyParser.json());
+app.use(express.json());
 app.use("/blog-post", blogRouter)
 
 let server;
 
-function runServer() {
-  const port = process.env.PORT || 8080;
-  return new Promise((resolve, reject) => {
-    server = app
-      .listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve(server);
-      })
-      .on("error", err => {
-        reject(err);
-      });
-  });
-}
+mongoose.Promise = global.Promise;
 
-function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log("Closing server");
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
+app.get('/posts', (req, res) => {
+  BlogPost
+  .find()
+  .exec()
+  .then(posts => {
+    res.json(posts.map(post => post.apiRepr()));
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({error: 'something went terribly wrong'});
+  });
+});
+
+app.get('/posts/:id', (res, res) => {
+  BlogPost
+  .findById(req.params.id)
+  .then(post => res.json(post.apiRepr()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({error: 'something went terribly wrong'})
+  });
+});
+
+app.post('/posts', (res, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  requiredFields.forEach(field => {
+    if (!(field in req.body)) {
+      res.status(400).json(
+        {error: `missing ${field} in request body`});
+    }});
+})
+
+BlogPost
+.create({
+  title: req.body.title,
+  content: req.body.content,
+  author: req.body.author
+})
+.then(blogPost => res.status(201).json(blogPost.apiRepr()))
+.catch(err => {
+  console.error(err);
+  res.status(500).json({error: 'Something went wrong'});
+});
+
+app.delete('/post/:id', (req, res) => {
+  BlogPost
+  .findByIdAndRemove(req.params.id)
+  .then(() => {
+    res.status(204).json({message: 'success'});
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({error: 'something went terribly wrong'})
+  });
+});
+
+app.put('/posts/:id', (res, req) => {
+  if (!(req.params.id && req.params.id === req.body.id)) {
+    res.status(400).json({
+      error: 'Request path id and request body id values must match'
+    });
+  }
+})
+
+const updateed = {};
+const updateedFields = ['title', 'content', 'author'];
+updateedFields.forEach(field => {
+  if (field in req.body) {
+    updateed[field] = req.body[field];
+  }
+})
+
+BlogPost
+.findByUpdate(req.params.id, {$set:updateed})
+.then(updateed => res.status(201).json(updatedPost.apiRepr()))
+.catch(err => res.status(500).json({message: 'Something went wrong'}));
+
+app.delete('/:id', (req, res) => {
+  BlogPost.delete(req.params.id);
+  console.log(`Deleted blog post with id ${req.params.ID}`);
+  res.status(204).end();
+});
+
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+})
+
+function runServer(callback) {
+  mongoose.connect(DATABASE_URL, (err) => {
+    if (err && callback) {
+      return callback(err);
+    }
+
+    app.listen(PORT, () => {
+      console.log(`Your app is listening on port ${PORT}`);
+      if (callback) {
+        callback();
       }
-      resolve();
     });
   });
-}
+};
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
-}
+  runServer(function(err) {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
 
-module.exports = { app, runServer, closeServer };
+module.exports = { app, runServer};
